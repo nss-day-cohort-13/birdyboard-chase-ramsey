6d670a9892254f497892fcb10528dc3e47ebd8a3
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-01 11:04:00 -0500

    Some planning with docstrings in main.py
    Initial files have been created, and basic classes have been created in
    main.py. No implementation, but some docstrings detailing functionality
    for later

---
+++ main.py
@@ -0,0 +1,45 @@
+class ChirpData:
+
+  def __init__(self):
+    # self.all_chirps = [load from chirps.txt]
+    pass
+
+class UserData:
+
+  def __init__(self):
+    """Creates the 'users' property, which loads a list
+       of users from the serialized text file, users.txt
+    """
+    # self.users = [load from users.txt]
+    pass
+
+  def new_user(self):
+    """Creates a new user object from user input
+    Method arguments: n/a
+    """
+    pass
+
+  def find_user(self, *args):
+    """Returns True/False if a user object is found in self.users
+       that matches the search arguments passed in
+       Method arguments
+       ================
+       search - username associated with the user object being searched for
+       password - password associated with the user object being searched for
+    """
+    pass
+
+class Chirp:
+
+  def __init__(self):
+    pass
+
+class PrivateChirp(Chirp):
+
+  def __init__(self):
+    pass
+
+class User:
+
+  def __init__(self):
+    pass
