af49d81521a2cff2c76f9d94e7965aadd774b18e
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-04 10:06:43 -0500

    Wrote check sign in function for user interface
    Set up submit_new_chirp to check sign in

--- interface.py
+++ interface.py
@@ -6,11 +6,31 @@ class UserInterface:
       input and passing it to the appropriate data class methods
   """
 
-  def __init__(self):
-    pass
+  def __init__(self, chirp_file, user_file):
+    """ On init, this class will get instances of ChripData and
+        UserData to run the program
+    """
+    self.chirp_data = ChirpData(chirp_file)
+    self.user_data = UserData(user_file)
 
+  def check_sign_in(self):
+    """ Checks to make sure a there is a current user;
+        returns True or False
+        Method arguments: n/a
+    """
+    if self.user_data.current_user == None:
+      print('You have to be signed in to do that. Sign in as an existing user or create a new user from the main menu.\n')
+      return False
+    else:
+      return True
+
+  # @check_sign_in
   def submit_new_chirp(self):
     """ Takes user input and submits it to create a new chirp
         Method arguments: n/a
     """
-    pass
+    login = self.check_sign_in()
+    if login == False:
+      return
+    else:
+      print('Got past sign in.')
